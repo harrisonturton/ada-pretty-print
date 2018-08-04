d0d8b3f132480bf8fb492888697ca0d96f59644d
Harrison Turton <harrisonturton@gmail.com>
2018-08-04 17:15:12 +1000

    Fixed styling, added comments & added Effect_Task

--- pretty_print.adb
+++ pretty_print.adb
@@ -5,9 +5,18 @@ with Ada.Strings.Unbounded;  use Ada.Strings.Unbounded;
 package body Pretty_Print is
 
   subtype ANSI_Code is String;
+
+  -- Pointer to the ANSI_Code. Needed for array
+  -- of variable-length strings below.
   type ANSI_Code_Handle is access constant ANSI_Code;
 
-  -- Colors
+  -- ANSI codes for colors
+  -- "aliased" puts the variable in memory, rather
+  -- than optimizing into a register. This allows us
+  -- to refer to it by a pointer, which lets us
+  -- have arrays of variable-length Strings without
+  -- using Ada.Strings.Unbounded, because it's actually an
+  -- array of pointers (sort-of ragged array).
   Black_Code   : aliased constant ANSI_Code := "30";
   Red_Code     : aliased constant ANSI_Code := "31";
   Green_Code   : aliased constant ANSI_Code := "32";
@@ -17,7 +26,9 @@ package body Pretty_Print is
   Cyan_Code    : aliased constant ANSI_Code := "36";
   White_Code   : aliased constant ANSI_Code := "37";
 
-  -- Effects
+  -- ANSI codes for text effects
+  -- See comment for the color ANSI codes for why
+  -- they're all "aliased" constants.
   Bold_Code       : aliased constant ANSI_Code := "1";
   Faint_Code      : aliased constant ANSI_Code := "2";
   Italic_Code     : aliased constant ANSI_Code := "3";
@@ -26,14 +37,13 @@ package body Pretty_Print is
   Fast_Blink_Code : aliased constant ANSI_Code := "6";
   Strike_Code     : aliased constant ANSI_Code := "9";
 
-  -- Must escape & prepend this code, otherwise the
-  -- styling will spill into other terminal output.
+  -- If we don't escape & prepend this reset code,
+  -- our styles will effect the whole terminal.
   Reset_Code   : String := "[0m";
 
-  -- Table of ANSI color codes
-  -- Use pointers because each string has different
-  -- length -- creates ragged array.
-  -- Consider using Unbounded strings everywhere.
+  -- Ragged array of ANSI color codes.
+  -- Array of pointers to get around Ada's fixed-length
+  -- string restrictions without using Ada.Strings.Unbounded.
   Color_Table : array (Color) of ANSI_Code_Handle :=
     (Black   => Black_Code'Access,
      Red     => Red_Code'Access,
@@ -44,6 +54,9 @@ package body Pretty_Print is
      Cyan    => Cyan_Code'Access,
      White   => White_Code'Access);
 
+  -- Ragged array of ANSI text effect codes.
+  -- See Color_Table comment for why it's an
+  -- array of pointers.
   Effect_Table : array (Effect) of ANSI_Code_Handle :=
     (Bold       => Bold_Code'Access,
      Faint      => Faint_Code'Access,
@@ -53,17 +66,18 @@ package body Pretty_Print is
      Fast_Blink => Fast_Blink_Code'Access,
      Strike     => Strike_Code'Access);
 
+  -- With_Color returns a String with the relevant in-band ANSI
+  -- codes to make it a certain color.
   function With_Color  (C : Color;  S : String) return String is
     (ESC & "[" & Color_Table(C).all & "m" & S & ESC & Reset_Code);
 
+  -- With_Effect returns a String with the relevant in-band ANSI
+  -- codes to give it a certain text effect.
   function With_Effect (E : Effect; S : String) return String is
     (ESC & "[" & Effect_Table(E).all & "m" & S & ESC & Reset_Code);
 
-  function Color_Task (Index : Positive; S : String) return String is
-  begin
-    return With_Color(Color'Val((Index mod No_Of_Tasks) + 1), S);
-  end;
-
+  -- With_Effect combines multiple effects, and applied the relevant
+  -- ANSI codes to style the String.
   function With_Effect (E : Effect_List; S : String) return String is
     Result    : Unbounded_String := To_Unbounded_String(ESC & "[");
     Str       : Unbounded_String := To_Unbounded_String(S);
@@ -81,9 +95,12 @@ package body Pretty_Print is
     return To_String(Result) & ESC & Reset_Code;
   end;
 
+  -- With_Style applies a Color & Effect to a String.
   function With_Style (C : Color; E : Effect; S : String) return String is
     (ESC & "[" & Color_Table(C).all & ";" & Effect_Table(E).all & "m" & S & ESC & Reset_Code);
 
+  -- With_Style applies a Color, and multiple combined text effects
+  -- to a String.
   function With_Style (C : Color; E : Effect_List; S : String) return String is
     Result    : Unbounded_String := To_Unbounded_String(ESC & "[" & Color_Table(C).all & ";");
     Str       : Unbounded_String := To_Unbounded_String(S);
@@ -101,6 +118,17 @@ package body Pretty_Print is
     return To_String(Result) & ESC & Reset_Code;
   end;
 
+  -- Color_Task colors the text depending on the index. Different index
+  -- means different color.
+  function Color_Task  (Index : Positive; S : String)  return String is
+    (With_Color(Color'Val((Index mod No_Of_Tasks) + 1), S));
+
+  -- Effect_Task applies a text effect depending on the index. Different
+  -- index means a differente text effect.
+  function Effect_Task (Index : Positive; S : String) return String is
+    (With_Effect(Effect'Val((Index mod No_Of_Tasks) + 1), S));
+
+  -- Each of these just makes the String a certain color.
   function Black   (S : String) return String is (With_Color(Black,  S));
   function Red     (S : String) return String is (With_Color(Red,    S));
   function Green   (S : String) return String is (With_Color(Green,  S));
@@ -110,6 +138,7 @@ package body Pretty_Print is
   function Cyan    (S : String) return String is (With_Color(Yellow, S));
   function White   (S : String) return String is (With_Color(Yellow, S));
 
+  -- Each of these just gives the String a certain effect.
   function Bold       (S : String) return String is (With_Effect(Bold,       S));
   function Faint      (S : String) return String is (With_Effect(Faint,      S));
   function Italic     (S : String) return String is (With_Effect(Italic,     S));
@@ -118,31 +147,37 @@ package body Pretty_Print is
   function Fast_Blink (S : String) return String is (With_Effect(Fast_Blink, S));
   function Strike     (S : String) return String is (With_Effect(Strike,     S));
 
+  -- Print a String with a certain color
   procedure Put_Color (C : in Color; S : in String) is
   begin
     Put_Line(With_Color(C, S));
   end;
 
+  -- Print a String with an text effect
   procedure Put_Effect (E : in Effect; S : in String) is
   begin
     Put_Line(With_Effect(E, S));
   end;
 
+  -- Print a String with multiple text effects
   procedure Put_Effect (E : in Effect_List; S : in String) is
   begin
     Put_Line(With_Effect(E, S));
   end;
 
+  -- Print a String with a color & text effect
   procedure Put_Style (C : in Color; E : in Effect; S : in String) is
   begin
     Put_Line(With_Style(C, E, S));
   end;
 
+  -- Print a String with a color & multiple text effects
   procedure Put_Style (C : in Color; E : in Effect_List; S : in String) is
   begin
     Put_Line(With_Style(C, E, S));
   end;
 
+  -- Print a String colored according to the Task Index.
   procedure Put_Task  (Index : in Positive; S : in String) is
   begin
     Put_Line(Color_Task(Index, "Task " & Positive'Image(Index) & ": " & S));
--- pretty_print.ads
+++ pretty_print.ads
@@ -22,15 +22,15 @@ package Pretty_Print is
      Fast_Blink,
      Strike);
 
+  -- Used to have multiple effects at the same time, i.e. (Bold, Underline)
   type Effect_List is array (Positive range <>) of Effect;
 
-  -- Style a string by a colour & any number of effects.
+  -- Style a String with colors & effects.
   function With_Style (C : Color; E : Effect; S : String) return String;
   function With_Style (C : Color; E : Effect_List; S : String) return String;
 
-  -- Colour a string
-  function With_Color  (C : Color;  S : String) return String;
-  function Color_Task  (Index : Positive; S : String) return String;
+  -- Colour a String. Helper function for each color.
+  function With_Color (C : Color;  S : String) return String;
   function Black   (S : String) return String;
   function Red     (S : String) return String;
   function Green   (S : String) return String;
@@ -40,22 +40,36 @@ package Pretty_Print is
   function Cyan    (S : String) return String;
   function White   (S : String) return String;
 
-  -- Put an effect on a string
+  -- Put an effect on a String - Bold, Underlined etc.
+  -- Can easily combine multiple effects.
   function With_Effect (E : Effect; S : String) return String;
   function With_Effect (E : Effect_List;  S : String) return String;
-  function Bold       (S : String) return String;
-  function Faint      (S : String) return String;
-  function Italic     (S : String) return String;
-  function Underline  (S : String) return String;
-  function Slow_Blink (S : String) return String;
-  function Fast_Blink (S : String) return String;
-  function Strike     (S : String) return String;
+  function Bold        (S : String) return String;
+  function Faint       (S : String) return String;
+  function Italic      (S : String) return String;
+  function Underline   (S : String) return String;
+  function Slow_Blink  (S : String) return String;
+  function Fast_Blink  (S : String) return String;
+  function Strike      (S : String) return String;
 
+  -- Applies a different color or effect depending on the
+  -- ID of the task.
+  function Color_Task  (Index : Positive; S : String) return String;
+  function Effect_Task (Index : Positive; S : String) return String;
+
+  -- Print a String in a specifc Color.
   procedure Put_Color  (C : in Color;  S : in String);
+
+  -- Print a string with one or more Effects.
   procedure Put_Effect (E : in Effect; S : in String);
   procedure Put_Effect (E : in Effect_List; S : in String);
+
+  -- Print a string with both a Color & one or more Effects.
   procedure Put_Style  (C : in Color;  E : in Effect; S : in String);
   procedure Put_Style  (C : in Color;  E : in Effect_List; S : in String);
+
+  -- Print a String styled depending on the index.
+  -- Intended to make debugging concurrent output simpler.
   procedure Put_Task   (Index : in Positive; S : in String);
 
 end;
